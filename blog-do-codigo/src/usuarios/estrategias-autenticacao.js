fb9c961f5ac958f0ac5f4041b53dc56277a81ddf
Cleilton Oliveira <clltnoliveira@gmail.com>
2022-04-06 13:42:35 -0300

    (blog-do-codigo) : Lesson 2 - Authentication strategy

---
+++ blog-do-codigo/src/usuarios/estrategias-autenticacao.js
@@ -0,0 +1,34 @@
+const passport = require('passport')
+const { InvalidArgumentError } = require('../erros')
+const LocalStrategy = require('passport-local').Strategy
+const Usuario = require('./usuarios-modelo')
+const bcrypt = require('bcrypt')
+
+function verificaUsuario(usuario) {
+    if (!usuario) {
+        throw new InvalidArgumentError('Não existe usuario')
+    }
+}
+
+async function verificaSenha(senha, senhaHash) {
+    const senhaValida = await bcrypt.compare(senha, senhaHash)
+    if (!senhaValida) {
+        throw new InvalidArgumentError('Não autenticado')
+    }
+}
+
+passport.use(
+    new LocalStrategy({
+        usernameField: 'email', passwordField: 'senha', session: false
+    },
+        async (email, senha, done) => {
+            try {
+                const usuario = await Usuario.buscaPorEmail(email)
+                verificaUsuario(usuario)
+                await verificaSenha(senha, usuario.senhaHash)
+                done(null, usuario)
+            } catch (error) {
+                done(error)
+            }
+        })
+)
